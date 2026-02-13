77202b3f79c76b42a3973fb74a2638a91175adc4
tanveerpy <dahajsainak@gmail.com>
2026-02-13 18:25:40 +0500

    fix: remove basePath for custom domain deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,7 @@ const withMDX = createMDX({
 });
 
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-    basePath: isProd ? "/loanrepaymentuk" : "",
     pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
     output: "export",
     images: {
